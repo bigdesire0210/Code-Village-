71a72d9fef80769262b91be644178438fd72f8ba
bigdesire0210 <bigdesire210@gmail.com>
2021-01-10 18:38:41 +0900

    8-2課題提出

--- ex08-2.js
+++ ex08-2.js
@@ -5,3 +5,18 @@ document.body.appendChild(newH1)
 const newP = document.createElement('p')
 newP.textContent = "JavaScriptからHTMLを扱うための仕組み"
 document.body.appendChild(newP)
+
+const newul = document.createElement('ul')
+document.body.appendChild(newul)
+
+const newli0 = document.createElement('li')
+newli0.textContent = 'documentオブジェクト'
+newul.appendChild(newli0)
+
+const newli1 = document.createElement('li')
+newli1.textContent = 'getElementById'
+newul.appendChild(newli1)
+
+const newli2 = document.createElement('li')
+newli2.textContent = 'イベントリスナ'
+newul.appendChild(newli2)
