73e1a914598e42bb4e10ea0e72deefb098a8537a
bigdesire0210 <bigdesire210@gmail.com>
2021-01-10 18:17:03 +0900

    課題提出の練習

---
+++ ex08-2.js
@@ -0,0 +1,7 @@
+const newH1 = document.createElement('h1')
+newH1.textContent = 'DOM'
+document.body.appendChild(newH1)
+
+const newP = document.createElement('p')
+newP.textContent = "JavaScriptからHTMLを扱うための仕組み"
+document.body.appendChild(newP)
